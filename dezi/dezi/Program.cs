044c404ed4376db297e2c795f5a96dbacaff5026
Yannis Kraemer <yannkra21@gmail.com>
2022-04-08 23:09:55 +0200

    Init C# project.

---
+++ dezi/dezi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dezi
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
