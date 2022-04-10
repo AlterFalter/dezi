a4b7caa7b7cd1dcdfbcb04976762e25a36ee800c
Yannis Kraemer <yannkra21@gmail.com>
2022-04-10 17:42:16 +0200

    Add UiElement class.

--- dezi/dezi/UiElements/Editor.cs
+++ dezi/dezi/UiElements/Editor.cs
@@ -13,12 +13,10 @@ namespace dezi.UiElements
     /// and creates the specific UI for editing the file
     /// but nothing around it, such as menu.
     /// </summary>
-    public class Editor
+    public class Editor : UiElement
     {
         private readonly KeyboardInputs keyboardInputs;
         private IList<Cursor> cursors;
-        private int width;
-        private int height;
 
         public IList<Cursor> Cursors
         {
@@ -32,44 +30,6 @@ namespace dezi.UiElements
         }
 
         public string Filepath { get; set; }
-
-        public bool IsInFocus { get; set; }
-
-        public int CoordinateX { get; set; }
-
-        public int CoordinateY { get; set; }
-
-        public int Width
-        {
-            get
-            {
-                return this.width;
-            }
-            set
-            {
-                this.width = value;
-                if (this.Buffer != null)
-                {
-                    this.Buffer.OutputWidth = this.width - 1;
-                }
-            }
-        }
-
-        public int Height
-        {
-            get
-            {
-                return this.height;
-            }
-            set
-            {
-                this.height = value;
-                if (this.Buffer != null)
-                {
-                    this.Buffer.OutputHeight = this.height - 1;
-                }
-            }
-        }
 
         public EditorBuffer Buffer { get; }
 
@@ -124,13 +84,13 @@ namespace dezi.UiElements
         {
             // TODO: make content responsive
             string bar = $"{this.Filepath} | Markdown | UTF-8";
-            if (bar.Length < this.width)
+            if (bar.Length < this.Width)
             {
                 bar = bar.PadRight(this.Width - bar.Length);
             }
-            else if (bar.Length > this.width)
+            else if (bar.Length > this.Width)
             {
-                bar = bar.Substring(0, this.width);
+                bar = bar.Substring(0, this.Width);
             }
             return bar;
         }
@@ -194,6 +154,21 @@ namespace dezi.UiElements
         public void RemoveDuplicateCursors()
         {
             this.cursors = Cursor.RemoveDuplicateCursors(this.Cursors);
+        }
+
+        public override void UpdateSubmoduleSizes(int widthDelta, int heightDelta)
+        {
+            if (this.Buffer != null)
+            {
+                if (widthDelta != 0)
+                {
+                    this.Buffer.OutputWidth = this.Width + widthDelta;
+                }
+                if (heightDelta != 0)
+                {
+                    this.Buffer.OutputHeight = this.Height + heightDelta;
+                }
+            }
         }
     }
 }
---
+++ dezi/dezi/UiElements/UiElement.cs
@@ -0,0 +1,44 @@
+﻿namespace dezi.UiElements
+{
+    public abstract class UiElement
+    {
+        private int width;
+        private int height;
+
+        public int Width
+        {
+            get
+            {
+                return this.width;
+            }
+            set
+            {
+                int widthDelta = value - this.width;
+                this.width = value;
+                UpdateSubmoduleSizes(widthDelta, 0);
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return this.height;
+            }
+            set
+            {
+                int heightDelta = value - this.height;
+                this.height = value;
+                UpdateSubmoduleSizes(0, heightDelta);
+            }
+        }
+
+        public int CoordinateX { get; set; }
+
+        public int CoordinateY { get; set; }
+
+        public bool IsInFocus { get; set; }
+
+        public abstract void UpdateSubmoduleSizes(int widthDelta, int heightDelta);
+    }
+}
